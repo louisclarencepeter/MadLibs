aa50c193374896a56903c1b1a1de9ad6f43193fc
Louis Peter <louisclarencepeters@gmail.com>
2024-01-11 09:30:25 +0100

    added a comment and title

--- MadLibsAdventure.cs
+++ MadLibsAdventure.cs
@@ -6,6 +6,18 @@ namespace MadLibs
     {
         static void Main(string[] args)
         {
+            /*
+            This program is Mad Libs, Mad Libs are short stories with blanks for the player to fill in that represent different parts of speech. The end result is a really hilarious and strange story.
+            Author: Louis
+            */
+
+            // Let the user know that the program is starting:
+            Console.WriteLine("Welcome to the whimsical world of Mad Libs!");
+
+            // Give the Mad Lib a title:
+            string title = "The Unexpected Adventure of Louis";
+            Console.WriteLine($"Title: {title}\n"); 
+
   
         }
     }
