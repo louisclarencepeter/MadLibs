22485aa332757cfb07a514ab92c7050d50d09303
Louis Peter <louisclarencepeters@gmail.com>
2024-01-11 09:32:09 +0100

    added the remaining variables

--- MadLibsAdventure.cs
+++ MadLibsAdventure.cs
@@ -16,9 +16,9 @@ namespace MadLibs
 
             // Give the Mad Lib a title:
             string title = "The Unexpected Adventure of Louis";
-            Console.WriteLine($"Title: {title}\n"); 
+            Console.WriteLine($"Title: {title}\n");
 
-             // Define user input and variables:
+            // Define user input and variables:
             Console.Write("Imagine an adjective as colorful as a rainbow: ");
             string adj1 = Console.ReadLine();
             Console.Write("Think of another adjective, as mysterious as the night: ");
@@ -32,7 +32,22 @@ namespace MadLibs
             Console.Write("Conjure another noun, something utterly bizarre: ");
             string noun2 = Console.ReadLine();
 
-  
+            Console.Write("Name an animal you'd like to be friends with: ");
+            string animal = Console.ReadLine();
+            Console.Write("What's a food that makes you go 'yum'? ");
+            string food = Console.ReadLine();
+            Console.Write("Pick a fruit from a magical orchard: ");
+            string fruit = Console.ReadLine();
+            Console.Write("Choose a superhero you'd have tea with: ");
+            string superhero = Console.ReadLine();
+            Console.Write("Select a country from a storybook map: ");
+            string country = Console.ReadLine();
+            Console.Write("What dessert do you dream about? ");
+            string dessert = Console.ReadLine();
+            Console.Write("Imagine a year in the future, any year: ");
+            string year = Console.ReadLine();
+
+
         }
     }
 }
